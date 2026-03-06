ed5922791a52ef5243391ffa817577730c769058
Alejandro Guerrero <alexrm1x@gmail.com>
2026-03-06 19:33:49 +0100

    "Step 2: HTML básico con formulario de subida"

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hola WORLD');
-});
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
